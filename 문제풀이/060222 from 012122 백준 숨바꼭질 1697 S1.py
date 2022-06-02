e648b87d26c9d88fce95f10f7f118ea694c91e00
Seungmin <haradwaith@naver.com>
2022-06-02 13:29:58 +0900

    [add] 문제풀이

---
+++ 문제풀이/060222 from 012122 백준 숨바꼭질 1697 S1.py
@@ -0,0 +1,51 @@
+'''
+숨바꼭질 성공다국어
+ 
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	128 MB	153411	43603	27309	25.023%
+문제
+수빈이는 동생과 숨바꼭질을 하고 있다. 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 
+동생은 점 K(0 ≤ K ≤ 100,000)에 있다. 수빈이는 걷거나 순간이동을 할 수 있다. 
+만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 
+순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
+
+수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
+
+입력
+첫 번째 줄에 수빈이가 있는 위치 N과 동생이 있는 위치 K가 주어진다. N과 K는 정수이다.
+
+출력
+수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
+
+예제 입력 1 
+5 17
+예제 출력 1 
+4
+힌트
+수빈이가 5-10-9-18-17 순으로 가면 4초만에 동생을 찾을 수 있다.
+'''
+import collections
+
+
+fr, to = map(int, input().split())
+if fr >= to:
+    print(fr-to)
+    exit()
+toVisit = [1]*200001
+toVisit[fr] = 0
+queue = collections.deque()
+queue.append((fr, 0))
+while queue:
+    pos, cos = queue.popleft()
+    if pos == to:
+        print(cos)
+        break
+    if pos and toVisit[pos-1]:
+        toVisit[pos-1] = 0
+        queue.append((pos-1, cos+1))
+    if pos < 200000 and toVisit[pos+1]:
+        toVisit[pos+1] = 0
+        queue.append((pos+1, cos+1))
+    if pos*2 <= 200000 and toVisit[pos*2]:
+        toVisit[pos*2] = 1
+        queue.append((pos*2, cos+1))
